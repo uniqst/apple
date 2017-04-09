17132112068db6bb50cca5292879cfa152a5184a
Cyanoluck <cyanoluck@gmail.com>
2017-04-09 16:12:18 +0300

    start

--- config/web.php
+++ config/web.php
@@ -9,7 +9,8 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'eeeerrrrggggssss333444',
+            'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',

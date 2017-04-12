ef6965fea08092d5238e78a735c2c0fb0e59b82a
Cyanoluck <cyanoluck@gmail.com>
2017-04-12 11:54:42 +0300

    product res fix + slider 0.1

--- views/site/index.php
+++ views/site/index.php
@@ -26,7 +26,38 @@ $this->title = 'My Yii Application';
         </div>
 
         <div class="col-sm-9 slider">
-            <div class="slide-content">fdf</div>
+            <div class="slide-content">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="images/new-pic4.jpg" alt="Chania">
+                        </div>
+
+                        <div class="item">
+                            <img src="img_chania2.jpg" alt="Chania">
+                        </div>
+
+                        <div class="item">
+                            <img src="img_flower.jpg" alt="Flower">
+                        </div>
+
+                        <div class="item">
+                            <img src="img_flower2.jpg" alt="Flower">
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
 
     </div>
